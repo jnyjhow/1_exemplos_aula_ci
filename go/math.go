f56598d53d1e003d0e71a7bb1296b70fe5c7f40e
jnyjhow <jhonatan_yamane@hotmail.com>
2022-01-13 17:08:09 -0300

    feat: :white_check_mark: adicionando sonarqube

---
+++ go/math.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(Soma(10, 5))
+
+}
+
+func Soma(a int, b int) int {
+	return a + b
+}
+
+func Subtracao(a int, b int) int {
+	return a - b
+}
+
+func Multiplicacao(a int, b int) int {
+	return a * b
+}
+
+func Divisao(a int, b int) int {
+	return a / b
+}
+
+func SomaX(a int, b int) int {
+	return a + b + a
+}
